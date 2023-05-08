228156aed4ff3bc73d1b36af0bce63063181f0c6
nawzaysfinah <nawzaysfinah@gmail.com>
2023-05-08 12:12:27 +0800

    5. removed elements from HTML, added via JS

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
+import * as module from "./initial-page-load.js";
+
 console.log("Hello, is me, a Mario! im an italian short fella");
 
-const content = document.querySelector("#content");
-
-// Add Page Content
-addPageContent();
-
-function addPageContent() {
-  const heading = document.createElement("h1");
-  heading.textContent("Welcome to FNS!");
-  heading.classList.add("heading");
-  content.appendChild(heading);
-}
-
-document.body.appendChild(content);
+module.initialPageLoad();
---
+++ src/initial-page-load.js
@@ -0,0 +1,26 @@
+export function initialPageLoad() {
+  const contentDiv = document.querySelector("#content");
+
+  const heading = document.createElement("h1");
+  heading.textContent = "Fermentation Nation Station";
+  heading.classList.add("landing-title");
+  contentDiv.appendChild(heading);
+
+  const topImage = document.createElement("img");
+  topImage.classList.add("top-image");
+  topImage.src = "../src/assets/germ.png";
+  topImage.alt = "it's a germ!";
+  contentDiv.appendChild(topImage);
+
+  const subtitle = document.createElement("h3");
+  subtitle.textContent =
+    "💀 some preservation, 🥢 for satiation, 📸 with decent presentation";
+  subtitle.classList.add("subtitle");
+  contentDiv.appendChild(subtitle);
+
+  const para = document.createElement("p");
+  para.textContent =
+    "FNS is my attempt at learning fermentation and how to incorporate it into my daily life, changing my consumption and building a healthier ecosystem of consumption and gut micro-biome.";
+  para.classList.add("landing-page-copy");
+  contentDiv.appendChild(para);
+}
