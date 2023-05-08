891cf2748df14185476c5e62636f8ade5dcef8fc
nawzaysfinah <nawzaysfinah@gmail.com>
2023-05-08 11:44:01 +0800

    basic HTML for FNS website

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello, is me, a Mario!");
+console.log("Hello, is me, a Mario! im an italian short fella");
+
+const content = document.querySelector("#content");
+
+// Add Page Content
+addPageContent();
+
+function addPageContent() {
+  const heading = document.createElement("h1");
+  heading.textContent("Welcome to FNS!");
+  heading.classList.add("heading");
+  content.appendChild(heading);
+}
+
+document.body.appendChild(content);
